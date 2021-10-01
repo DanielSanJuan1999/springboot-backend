69f8e2f50d24b9592f20734a72997120429231da
DanielSanJuan <dan.sanjuan@alumnos.duoc.cl>
2021-10-01 19:26:55 -0300

    Bug fixing application.properties and fix ProductRestController

--- spring-boot-backend/src/main/java/com/daniel/springbootbackend/controllers/ProductRestController.java
+++ spring-boot-backend/src/main/java/com/daniel/springbootbackend/controllers/ProductRestController.java
@@ -5,17 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 import com.daniel.springbootbackend.models.entity.Product;
 import com.daniel.springbootbackend.models.entity.services.IProductServices;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api")
 public class ProductRestController {
     
     @Autowired
     private IProductServices productServices;
 
+    @GetMapping("/products")
     public List<Product> index(){
         return productServices.findAll();
     }
